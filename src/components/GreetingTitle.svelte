b7829ed97da528058c83ca0521e9208f2f4930f9
Muthaiyan Mani <151814981+muthaiyan-mb-13966@users.noreply.github.com>
2024-12-20 17:53:37 +0530

    dwd

--- src/components/GreetingTitle.svelte
+++ src/components/GreetingTitle.svelte
@@ -7,7 +7,7 @@
   if (currentHour >= 5 && currentHour < 12) {
     greeting = "Good morning";
   } else if (currentHour >= 12 && currentHour < 18) {
-    greeting = "Good afternoon";
+    greeting = "Good afternoon Ragull";
   } else {
     greeting = "Good night";
   }
